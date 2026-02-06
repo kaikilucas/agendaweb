04a7bccd675f638f4949d5f635805b8af4c5fc12
kaikilucas <kaiki.lucasf@gmail.com>
2026-02-06 12:15:39 -0300

    Adiciona serviço de frontend no Node e rota raiz com index.html no localhost

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); // precisa para servir arquivos
 const app = express();
 
 const authRoutes = require("./routes/auth.routes");
 
 app.use(cors()); // 👈 ESSENCIAL
 app.use(express.json());
-
+// Rotas da API
 app.use("/api", authRoutes);
+
+// Servir frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// Rota raiz
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 module.exports = app;
